83bc2eb7289184cdc87accd7643b7cbea25e222d
Akash Kumar <v-akashkumar+expedia@expediagroup.com>
2025-08-09 18:26:16 +0530

    fix admin issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from 'tailwindcss'
+
+export default {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -138,8 +139,4 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-  ],
-}
+} satisfies Config
